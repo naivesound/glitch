0a27c9e47fec7d5b7950b6a68dfe220496ca5907
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2017-03-02 16:23:13 +0200

    rewritten main loop as a client/server osc model

--- src/main.cpp
+++ src/main.cpp
@@ -1,81 +1,22 @@
-#include <dirent.h>
-#include <signal.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <atomic>
+#include <fstream>
+#include <iostream>
+#include <mutex>
+#include <set>
+#include <thread>
+#include <vector>
 
-#include "sys.h"
+#include <dirent.h>
+#include <sys/stat.h>
+
 #include "vendor/RtAudio.h"
 #include "vendor/RtMidi.h"
+#include "vendor/oscpkt.hh"
+#include "vendor/udp.hh"
 #include "wav.h"
 
 extern "C" {
 #include "glitch.h"
-}
-
-static volatile sig_atomic_t sigusr = 0;
-static volatile sig_atomic_t sigint = 0;
-static mutex_t mutex;
-
-static char *script_path = NULL;
-static char *script = NULL;
-
-static void sigusr_cb(int signo) { sigusr = 1; }
-
-static void sigint_cb(int signo) { sigint = 1; }
-
-struct midi_key {
-  unsigned char key;
-  unsigned char velocity;
-};
-
-static void midi_cb(double dt, std::vector<unsigned char> *msg, void *context) {
-  static int last_note = -1;
-  struct glitch *g = (struct glitch *)context;
-  mutex_lock(&mutex);
-  if (msg->size() == 3) {
-    glitch_midi(g, msg->at(0), msg->at(1), msg->at(2));
-  }
-  mutex_unlock(&mutex);
-}
-
-static void reload(struct glitch *g) {
-  long size;
-  FILE *f = fopen(script_path, "r");
-  if (f == NULL) {
-    fprintf(stderr, "glitch: failed to open %s\n", script_path);
-    return;
-  }
-  fseek(f, 0, SEEK_END);
-  size = ftell(f);
-  fseek(f, 0, SEEK_SET);
-  script = (char *)realloc(script, size + 1);
-  fread(script, 1, size, f);
-  fclose(f);
-  script[size] = '\0';
-
-  mutex_lock(&mutex);
-  if (glitch_compile(g, script, strlen(script)) < 0) {
-    fprintf(stderr, "glitch: syntax error\n");
-  }
-  mutex_unlock(&mutex);
-}
-
-static int glitch_cb(void *out, void *in, unsigned int frames, double t,
-                     RtAudioStreamStatus status, void *context) {
-  struct glitch *g = (struct glitch *)context;
-  float *buf = (float *)out;
-  mutex_lock(&mutex);
-  for (int i = 0; i < frames; i++) {
-    float v = glitch_eval(g);
-    /* 2 output channels: copy the same sample twice */
-    *buf++ = v;
-    *buf++ = v;
-  }
-  mutex_unlock(&mutex);
-  return 0;
 }
 
 #define SAMPLES_DIR "samples"
@@ -184,66 +125,358 @@ static void load_samples() {
         wav_sample_sort);
 }
 
-static void usage(char *app) {
-  fprintf(stderr, "Glitch " VERSION " - minimal algorithmic music maker\n\n"
-                  "USAGE: %s [options ...] file\n\n"
-                  "  -d <device>     Audio output device name\n"
-                  "  -m <device>     MIDI-in device name\n"
-                  "  -r <rate>       Audio sample rate\n"
-                  "  -b <size>       Audio buffer size\n"
-                  /*"  -w <file>       Write output to a WAV file\n"*/
-                  "\n",
-          app);
-  try {
-    RtAudio *audio = new RtAudio();
-    RtMidiIn *midi_in = new RtMidiIn();
-    fprintf(stderr, "  Audio devices:\n");
-    for (int i = 0; i < audio->getDeviceCount(); i++) {
-      RtAudio::DeviceInfo info = audio->getDeviceInfo(i);
-      fprintf(stderr, "    %s\n", info.name.c_str());
-    }
-    fprintf(stderr, "  MIDI devices:\n");
-    for (int i = 0; i < midi_in->getPortCount(); i++) {
-      std::string name = midi_in->getPortName(i);
-      fprintf(stderr, "    %s\n", name.c_str());
-    }
-  } catch (RtAudioError &error) {
-    error.printMessage();
+class Glitch {
+public:
+  Glitch() {
+    g = glitch_create();
+    play("");
   }
+
+  ~Glitch() {
+    closeMIDI();
+    closeAudio();
+    glitch_destroy(g);
+  }
+
+  int openAudio(int index = -1, unsigned int sampleRate = 44100,
+                unsigned int numChannels = 2, unsigned int bufsz = 512) {
+    int r = 0;
+    m.lock();
+    closeAudio();
+    try {
+      RtAudio::StreamParameters params;
+      RtAudio::StreamOptions options;
+
+      audio = new RtAudio();
+      audio->showWarnings(false);
+      if (index == -1) {
+        index = audio->getDefaultOutputDevice();
+        options.flags |= RTAUDIO_ALSA_USE_DEFAULT;
+      }
+      params.deviceId = index;
+      params.nChannels = numChannels;
+      this->numChannels = numChannels;
+
+      audio->openStream(&params, NULL, RTAUDIO_FLOAT32, sampleRate, &bufsz,
+                        [](void *out, void *in, unsigned int frames, double t,
+                           RtAudioStreamStatus status, void *context) {
+                          Glitch *g = (Glitch *)context;
+                          float *buf = (float *)out;
+                          g->m.lock();
+                          for (int i = 0; i < frames; i++) {
+                            float v = glitch_eval(g->g);
+                            for (int i = 0; i < g->numChannels; i++) {
+                              *buf++ = v;
+                            }
+                          }
+                          g->m.unlock();
+                          return 0;
+                        },
+                        this, &options);
+      audio->startStream();
+
+      glitch_sample_rate(sampleRate);
+    } catch (RtAudioError &err) {
+      std::cerr << err.what() << std::endl;
+      closeAudio();
+      r = -1;
+    }
+    m.unlock();
+    return r;
+  }
+
+  void closeAudio() {
+    m.lock();
+    if (audio != NULL) {
+      if (audio->isStreamOpen()) {
+        audio->closeStream();
+      }
+      delete audio;
+      audio = NULL;
+    }
+    m.unlock();
+  }
+
+  int openMIDI(std::set<int> indices = std::set<int>()) {
+    int r = 0;
+    m.lock();
+    closeMIDI();
+    RtMidiIn rt;
+    try {
+      for (int i = 0; i < rt.getPortCount(); i++) {
+        if (indices.empty() || indices.find(i) != indices.end()) {
+          RtMidiIn *m = new RtMidiIn();
+          m->openPort(i);
+          m->setCallback(
+              [](double time, std::vector<unsigned char> *msg, void *arg) {
+                Glitch *g = (Glitch *)arg;
+                if (msg->size() == 3) {
+                  g->midi(msg->at(0), msg->at(1), msg->at(2));
+                }
+              },
+              this);
+          midiInputs.push_back(m);
+        }
+      }
+    } catch (RtAudioError &err) {
+      std::cerr << err.what() << std::endl;
+      r = -1;
+    }
+    m.unlock();
+    return r;
+  }
+
+  void closeMIDI() {
+    m.lock();
+    for (auto m : midiInputs) {
+      m->closePort();
+      delete m;
+    }
+    midiInputs.clear();
+    m.unlock();
+  }
+
+  int play(std::string s) {
+    m.lock();
+    int r = glitch_compile(g, s.c_str(), s.length());
+    if (r == 0) {
+      currentScript = s;
+    }
+    m.unlock();
+    return r;
+  }
+
+  void midi(unsigned char cmd, unsigned char a, unsigned char b) {
+    m.lock();
+    glitch_midi(g, cmd, a, b);
+    m.unlock();
+  }
+
+  static std::vector<std::string> listAudio() {
+    std::vector<std::string> devices;
+    try {
+      RtAudio rt;
+      rt.showWarnings(false);
+      for (int i = 0; i < rt.getDeviceCount(); i++) {
+        RtAudio::DeviceInfo info = rt.getDeviceInfo(i);
+        devices.push_back(info.name);
+      }
+    } catch (RtAudioError &err) {
+      std::cerr << err.getMessage() << std::endl;
+    }
+    return devices;
+  }
+
+  static std::vector<std::string> listMIDI() {
+    std::vector<std::string> devices;
+    try {
+      RtMidiIn rt;
+      for (int i = 0; i < rt.getPortCount(); i++) {
+        devices.push_back(rt.getPortName(i));
+      }
+    } catch (RtAudioError &err) {
+      std::cerr << err.getMessage() << std::endl;
+    }
+    return devices;
+  }
+
+private:
+  std::recursive_mutex m;
+  std::string currentScript;
+  unsigned int numChannels;
+  struct glitch *g = NULL;
+  RtAudio *audio = NULL;
+  std::vector<RtMidiIn *> midiInputs;
+};
+
+static void serverSendResult(oscpkt::UdpSocket *s, std::string uri, int r) {
+  oscpkt::PacketWriter pw;
+  oscpkt::Message result;
+  result.init(uri).pushInt32(r);
+  pw.init().addMessage(result);
+  s->sendPacketTo(pw.packetData(), pw.packetSize(), s->packetOrigin());
+}
+
+static int clientSendCommand(oscpkt::UdpSocket &s, oscpkt::Message &req,
+                             std::string res) {
+  oscpkt::PacketWriter pw;
+  pw.startBundle().startBundle().addMessage(req).endBundle().endBundle();
+  if (!s.sendPacket(pw.packetData(), pw.packetSize())) {
+    std::cerr << "client, send packet failed" << std::endl;
+    return -1;
+  }
+  if (s.receiveNextPacket(30)) { // timeout, ms
+    oscpkt::PacketReader pr(s.packetData(), s.packetSize());
+    oscpkt::Message *m;
+    while (pr.isOk() && (m = pr.popMessage()) != 0) {
+      if (m->match(res)) {
+        return 0;
+      }
+    }
+  }
+  std::cerr << "client, no response" << std::endl;
+  return -2;
+}
+
+static void serverLoop(oscpkt::UdpSocket *s) {
+  bool audioInitialized = false;
+  bool midiInitialized = false;
+  Glitch g;
+
+  while (s->isOk()) {
+    if (s->receiveNextPacket()) {
+      oscpkt::PacketReader pr;
+      pr.init(s->packetData(), s->packetSize());
+      oscpkt::Message *msg;
+      while (pr.isOk() && (msg = pr.popMessage()) != 0) {
+
+        // Open another audio device
+        if (msg->match("/glitch/settings/audio")) {
+          int deviceID, sampleRate, numChannels, bufSize;
+          if (msg->arg()
+                  .popInt32(deviceID)
+                  .popInt32(sampleRate)
+                  .popInt32(numChannels)
+                  .popInt32(bufSize)
+                  .isOkNoMoreArgs()) {
+            audioInitialized = true;
+            int r = g.openAudio(deviceID, sampleRate, numChannels, bufSize);
+            serverSendResult(s, "/glitch/status/audio", r);
+            std::cerr << "opened audio devices: " << r << std::endl;
+          }
+        }
+
+        // Open another set of MIDI devices
+        if (msg->match("/glitch/settings/midi")) {
+          std::set<int> ids;
+          int r = 0;
+          midiInitialized = true;
+          auto arg = msg->arg();
+          while (arg.nbArgRemaining() > 0) {
+            if (!arg.isInt32()) {
+              r = -1;
+            } else {
+              int id;
+              arg = arg.popInt32(id);
+              ids.insert(id);
+            }
+          }
+          if (r == 0) {
+            g.openMIDI(ids);
+          }
+          serverSendResult(s, "/glitch/status/midi", r);
+          std::cerr << "opened midi devices: " << r << std::endl;
+        }
+
+        // Play another script
+        if (msg->match("/glitch/play")) {
+          std::string script;
+          if (msg->arg().popStr(script).isOkNoMoreArgs()) {
+            if (!audioInitialized) {
+              audioInitialized = true;
+              g.openAudio();
+            }
+            if (!midiInitialized) {
+              midiInitialized = true;
+              g.openMIDI();
+            }
+            int r = g.play(script);
+            serverSendResult(s, "/glitch/status/play", r);
+            std::cerr << "updated script: " << r << std::endl;
+          }
+        }
+      }
+    }
+  }
+  std::cerr << "server done: " << s->errorMessage() << std::endl;
+}
+
+static void listDevices(std::string prefix) {
+  auto audio = Glitch::listAudio();
+  std::cout << prefix << "Audio devices:" << std::endl;
+  for (auto &s : audio) {
+    std::cout << prefix << "  " << &s - &audio[0] << ": " << s << std::endl;
+  }
+  std::cout << std::endl;
+
+  std::cout << prefix << "MIDI devices:" << std::endl;
+  auto midi = Glitch::listMIDI();
+  for (auto &s : midi) {
+    std::cout << prefix << "  " << &s - &midi[0] << ": " << s << std::endl;
+  }
+  std::cout << std::endl;
+}
+
+static void usage(const char *app) {
+  std::cout << std::endl;
+  std::cout << "USAGE: " << app << " [options] <script>" << std::endl;
+  std::cout << std::endl;
+  std::cout << "    -d <device>  Audio device" << std::endl;
+  std::cout << "    -b <frames>  Audio buffer size" << std::endl;
+  std::cout << "    -r <rate>    Audio sample rate" << std::endl;
+  std::cout << "    -n <chan>    Number of audio channels" << std::endl;
+  std::cout << "    -m <device>  MIDI device(s)" << std::endl;
+  std::cout << "    -w           Watch script file for changes" << std::endl;
+  std::cout << "    -P <port>    listen on OSC/UDP port" << std::endl;
+  std::cout << "    -p <port>    connect to OSC/UDP port" << std::endl;
+  std::cout << std::endl;
+
+  listDevices("  ");
 }
 
 int main(int argc, char *argv[]) {
-  int err = 0;
-  int opt;
-  struct glitch *g = NULL;
-  char *device = NULL;
-  const char *midi = "";
-  unsigned int bufsz = 512;
+  char opt;
 
-  int sample_rate = 48000;
+  int audioDeviceID = -1;
+  int sampleRate = 44100;
+  int numChannels = 1;
+  int bufSize = 1024;
 
-  RtAudio *audio = NULL;
-  RtMidiIn *midi_in = NULL;
-  RtMidiOut *midi_out = NULL;
-  RtAudio::StreamParameters params;
-  RtAudio::StreamOptions options;
-  RtAudio::DeviceInfo info;
+  std::set<int> midiDeviceIDs;
 
-  while ((opt = getopt(argc, argv, "d:m:b:r:h")) != -1) {
+  unsigned int serverPort = 0;
+  unsigned int clientPort = 0;
+
+  std::string filename = "";
+  bool watch = false;
+
+  bool hasAudioOptions = false;
+  bool hasMIDIOptions = false;
+
+  while ((opt = getopt(argc, argv, "d:r:n:b:m:p:P:wlh")) != -1) {
     switch (opt) {
     case 'd':
-      device = optarg;
+      hasAudioOptions = true;
+      audioDeviceID = atoi(optarg);
       break;
     case 'm':
-      midi = optarg;
+      hasMIDIOptions = true;
+      midiDeviceIDs.insert(atoi(optarg));
       break;
     case 'b':
-      bufsz = atoi(optarg);
+      hasAudioOptions = true;
+      bufSize = atoi(optarg);
+      break;
+    case 'n':
+      hasAudioOptions = true;
+      numChannels = atoi(optarg);
       break;
     case 'r':
-      sample_rate = atoi(optarg);
-      glitch_sample_rate(sample_rate);
+      hasAudioOptions = true;
+      sampleRate = atoi(optarg);
       break;
+    case 'p':
+      clientPort = atoi(optarg);
+      break;
+    case 'P':
+      serverPort = atoi(optarg);
+      break;
+    case 'w':
+      watch = true;
+      break;
+    case 'l':
+      listDevices("");
+      exit(0);
     case 'h':
     default:
       usage(argv[0]);
@@ -251,109 +484,104 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  if (optind != argc - 1) {
+  if (optind == argc - 1) {
+    filename = argv[optind];
+  }
+
+  if (filename == "" && clientPort == 0 && serverPort == 0) {
     usage(argv[0]);
     return 1;
   }
 
-  script_path = argv[optind];
-  time_t last_mtime = mtime(script_path);
-
   load_samples();
 
-  mutex_create(&mutex);
+  oscpkt::UdpSocket server;
+  std::thread *serverThread = NULL;
 
-  g = glitch_create();
-  glitch_compile(g, "", 0);
-
-  reload(g);
-
-  try {
-    audio = new RtAudio();
-    midi_in = new RtMidiIn();
-    midi_out = new RtMidiOut();
-  } catch (RtAudioError &error) {
-    error.printMessage();
-    err = 1;
-    goto cleanup;
+  if (serverPort != 0 && clientPort != 0 && serverPort != clientPort) {
+    std::cerr
+        << "both client and server ports are provided, but they don't match"
+        << std::endl;
+    usage(argv[0]);
+    exit(1);
   }
 
-  if (device == NULL) {
-    params.deviceId = audio->getDefaultOutputDevice();
-    options.flags |= RTAUDIO_ALSA_USE_DEFAULT;
-    info = audio->getDeviceInfo(params.deviceId);
-  } else {
-    params.deviceId = -1;
-    for (int i = 0; i < audio->getDeviceCount(); i++) {
-      RtAudio::DeviceInfo info = audio->getDeviceInfo(i);
-      if (info.name == device) {
-        params.deviceId = i;
-        break;
+  // If server port is provided or both ports are missing - start a server
+  if (serverPort != 0 || (serverPort == 0 && clientPort == 0)) {
+    server.bindTo(serverPort);
+    if (!server.isOk()) {
+      std::cerr << "failed to start server" << std::endl;
+      exit(1);
+    }
+    std::cerr << "started server on port " << server.boundPort() << std::endl;
+    clientPort = server.boundPort();
+    serverThread = new std::thread(serverLoop, &server);
+  }
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+  if (filename != "" || hasAudioOptions || hasMIDIOptions) {
+    std::cerr << "starting client to port " << clientPort << std::endl;
+    oscpkt::UdpSocket client;
+    client.connectTo("localhost", clientPort);
+    if (!client.isOk()) {
+      std::cerr << "failed to start client" << std::endl;
+      exit(1);
+    }
+
+    if (hasAudioOptions) {
+      oscpkt::Message req("/glitch/settings/audio");
+      req.pushInt32(audioDeviceID)
+          .pushInt32(sampleRate)
+          .pushInt32(numChannels)
+          .pushInt32(bufSize);
+      if (clientSendCommand(client, req, "/glitch/status/audio") < 0) {
+        std::cerr << "failed to change audio settings" << std::endl;
+        exit(1);
       }
     }
-  }
 
-  params.nChannels = 2;
-  params.firstChannel = 0;
-
-#if !defined(_WIN32)
-  signal(SIGUSR1, sigusr_cb);
-#endif
-  signal(SIGINT, sigint_cb);
-
-  midi_out->openVirtualPort("Glitch MIDI Output");
-
-  if (midi != NULL) {
-    for (int i = 0; i < midi_in->getPortCount(); i++) {
-      std::string name = midi_in->getPortName(i);
-      if (strlen(midi) == 0 || name == midi) {
-        RtMidiIn *midi_in = new RtMidiIn();
-        midi_in->openPort(i);
-        midi_in->setCallback(&midi_cb, g);
+    if (hasMIDIOptions) {
+      oscpkt::Message req("/glitch/settings/midi");
+      for (int id : midiDeviceIDs) {
+        req = req.pushInt32(id);
+      }
+      if (clientSendCommand(client, req, "/glitch/status/midi") < 0) {
+        std::cerr << "failed to change MIDI settings" << std::endl;
+        exit(1);
       }
     }
+
+    if (filename != "") {
+      time_t last_mtime = 0;
+      do {
+        struct stat st;
+        if (stat(filename.c_str(), &st) == 0 && st.st_mtime != last_mtime) {
+          last_mtime = st.st_mtime;
+          try {
+            std::ifstream ifs(filename);
+            std::string script((std::istreambuf_iterator<char>(ifs)),
+                               (std::istreambuf_iterator<char>()));
+            oscpkt::Message req("/glitch/play");
+            req.pushStr(script);
+            if (clientSendCommand(client, req, "/glitch/status/play") < 0) {
+              std::cerr << "failed to send script" << std::endl;
+            } else {
+              std::cerr << "script updated" << std::endl;
+            }
+          } catch (std::runtime_error &err) {
+            std::cerr << err.what() << std::endl;
+          } catch (std::exception &err) {
+            std::cerr << err.what() << std::endl;
+          }
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      } while (watch);
+    }
   }
 
-  try {
-    audio->openStream(&params, NULL, RTAUDIO_FLOAT32, sample_rate, &bufsz,
-                      glitch_cb, g, &options);
-    audio->startStream();
-  } catch (RtAudioError &error) {
-    error.printMessage();
-    err = 1;
-    goto cleanup;
+  if (serverThread != NULL) {
+    serverThread->join();
   }
-
-  while (audio->isStreamRunning()) {
-    sleep_ms(100);
-    if (sigint) {
-      audio->stopStream();
-      break;
-    }
-    time_t t = mtime(script_path);
-    if (sigusr || last_mtime != t) {
-      sigusr = 0;
-      last_mtime = t;
-      reload(g);
-    }
-  }
-
-cleanup:
-  if (g != NULL) {
-    glitch_destroy(g);
-  }
-  if (midi_in != NULL) {
-    delete midi_in;
-  }
-  if (midi_out != NULL) {
-    delete midi_out;
-  }
-  if (audio != NULL) {
-    if (audio->isStreamOpen()) {
-      audio->closeStream();
-    }
-    delete audio;
-  }
-  mutex_destroy(&mutex);
-  return err;
+  return 0;
 }
