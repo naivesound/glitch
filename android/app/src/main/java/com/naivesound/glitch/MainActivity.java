9b953b7a3f98ef934dd13d6fa4a4b6f784c99ea5
Anna Kruglik <kruglik.anna@gmail.com>
2017-07-02 19:16:03 +0300

    loaded presets from external storage and synced changes made in the app

--- android/app/src/main/java/com/naivesound/glitch/MainActivity.java
+++ android/app/src/main/java/com/naivesound/glitch/MainActivity.java
@@ -1,24 +1,33 @@
 package com.naivesound.glitch;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
 import android.os.Bundle;
-import android.text.InputType;
+import android.os.Environment;
+import android.os.Handler;
 import android.util.Log;
 import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethod;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Adapter;
 import android.widget.LinearLayout;
+
+import java.io.*;
+import java.util.*;
 
 import static trikita.anvil.DSL.*;
 
+import trikita.anvil.RenderableAdapter;
 import trikita.anvil.RenderableView;
 
 public class MainActivity extends Activity {
+
+    private final static String TAG = "Glitch";
+    private final static int REQUEST_PERMISSIONS_CODE = 1001;
 
     /* Prefer 44.1k to 48k, otherwise look for the highest supported rate */
     public final static int[] SAMPLE_RATES = {22050, 44100, 48000, 32000, 24000,
@@ -37,12 +46,31 @@ public class MainActivity extends Activity {
     private Glitch mGlitch;
     private Thread mAudioThread;
 
+    private Handler mHandler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+                Log.d(TAG, "Permissions are not granted. Request");
+
+                requestPermissions(
+                        new String[] {
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        },
+                        REQUEST_PERMISSIONS_CODE);
+                return;
+            }
+        }
+
+        Log.d(TAG, "Permissions are already granted. Load UI");
         mGlitch = new Glitch();
-        setContentView(new GlitchView(this));
+        setContentView(new MainView(this));
     }
 
     @Override
@@ -51,12 +79,23 @@ public class MainActivity extends Activity {
         super.onDestroy();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult(): requestCode = " + requestCode);
+        if (requestCode == REQUEST_PERMISSIONS_CODE &&
+                grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Permissions are granted. Proceed");
+            mGlitch = new Glitch();
+            setContentView(new MainView(this));
+        }
+    }
+
     private AudioTrack findAudioTrack() {
         for (int rate : SAMPLE_RATES) {
             for (int fmt : SAMPLE_FORMAT) {
                 for (int config : SAMPLE_CHANNEL_OUT) {
                     try {
-                        Log.d("Glitch", "Attempting rate " + rate +
+                        Log.d(TAG, "Attempting rate " + rate +
                                             "Hz, bits: " + fmt + ", channel: " +
                                             config);
                         int bufferSize =
@@ -70,7 +109,7 @@ public class MainActivity extends Activity {
                                 return track;
                         }
                     } catch (Exception e) {
-                        Log.e("Glitch", rate + "Exception, keep trying.", e);
+                        Log.e(TAG, rate + "Exception, keep trying.", e);
                     }
                 }
             }
@@ -86,9 +125,9 @@ public class MainActivity extends Activity {
     // Thread playbackThread = new Thread() {
     // public void run() {
     // int minBufferSize = AudioTrack.getMinBufferSize(
-    // track.getSampleRate(), track.getChannelConfiguration(),
+    // track.getPresetRate(), track.getChannelConfiguration(),
     // track.getAudioFormat());
-    // Glitch.setSampleRate(track.getSampleRate());
+    // Glitch.setPresetRate(track.getPresetRate());
     // boolean stereo = (track.getChannelConfiguration() ==
     // AudioFormat.CHANNEL_OUT_STEREO);
     // int numFrames = minBufferSize / 2;
@@ -96,7 +135,7 @@ public class MainActivity extends Activity {
     // short[] buffer = new short[stereo ? numFrames * 2 : numFrames];
     // Log.d("Glitch",
     //"playback started, buffer = " + minBufferSize + " " +
-    //(1000 * minBufferSize / track.getSampleRate()) +
+    //(1000 * minBufferSize / track.getPresetRate()) +
     //"ms");
     // track.play();
     // while (!Thread.interrupted()) {
@@ -121,7 +160,134 @@ public class MainActivity extends Activity {
     // return playbackThread;
     //}
 
+    public class MainView extends RenderableView {
+
+        private final static String SAMPLE_DIR = "glitch-presets";
+        // sync edited preset code with its original file in 1.5 sec after end of active editing
+        private final static long SYNC_SAMPLE_CODE_DELAY = 1500;
+
+        private List<String> mPresets = new ArrayList<>();
+        private String mPresetDirPath;
+        private String mPresetCode = "";
+        private int mSelectedPreset;
+
+        public MainView(Context context) {
+            super(context);
+            fetchPresetList();
+        }
+
+        private Adapter mPresetAdapter = RenderableAdapter.withItems(mPresets, (index, item) -> {
+            textView(() -> {
+                size(FILL, dip(48));
+                padding(dip(5), 0);
+                gravity(LEFT|CENTER_VERTICAL);
+                singleLine(true);
+                text(item);
+            });
+        });
+
+        @Override
+        public void view() {
+            if (mPresetCode.length() == 0 && mPresets.size() > 0) {
+                loadPreset(mPresets.get(mSelectedPreset));
+            }
+            linearLayout(() -> {
+                size(FILL, FILL);
+                orientation(LinearLayout.VERTICAL);
+                padding(dip(5));
+
+                spinner(() -> {
+                    init(() -> onItemSelected((a, v, pos, id) -> {
+                        loadPreset(mPresets.get(pos));
+                        mSelectedPreset = pos;
+                    }));
+                    size(FILL, WRAP);
+                    margin(0, 0, 0, dip(10));
+                    adapter(mPresetAdapter);
+                });
+
+                editText(() -> {
+                    size(FILL, 0);
+                    weight(1f);
+                    singleLine(false);
+                    imeOptions(EditorInfo.IME_FLAG_NO_ENTER_ACTION);
+                    gravity(TOP|LEFT);
+                    text(mPresetCode);
+                    onTextChanged(s -> {
+                        mHandler.removeCallbacks(mSyncPresetRunnable);
+                        if (!mGlitch.compile(s.toString())) {
+                            Log.d(TAG, "syntax: error");
+                        } else {
+                            mPresetCode = s.toString();
+                            Log.d(TAG, "syntax: ok");
+                            mHandler.postDelayed(mSyncPresetRunnable, SYNC_SAMPLE_CODE_DELAY);
+                        }
+                    });
+                });
+            });
+        }
+
+        private Runnable mSyncPresetRunnable = () -> {
+            Log.d(TAG, "updating preset code in file " + mPresets.get(mSelectedPreset));
+            File f = new File(mPresetDirPath, mPresets.get(mSelectedPreset));
+            FileWriter writer = null;
+            try {
+                writer = new FileWriter(f, false);
+                writer.write(mPresetCode + "\n");
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (writer != null) {
+                    try { writer.close(); } catch (IOException ex) {}
+                }
+            }
+        };
+
+        private void fetchPresetList() {
+            File root = Environment.getExternalStorageDirectory();
+
+            if (!Environment.getExternalStorageState(root).equals(Environment.MEDIA_MOUNTED)) {
+                throw new RuntimeException("Media storage is unmounted!");
+            }
+
+            File presetDir = new File(root, SAMPLE_DIR);
+            presetDir.mkdir();
+            mPresetDirPath = presetDir.getAbsolutePath();
+
+            File[] files = presetDir.listFiles();
+            if (files == null) {
+                throw new RuntimeException("Preset directory is empty");
+            }
+            for (File f : files) {
+                if (!f.isDirectory()) {
+                    mPresets.add(f.getName());
+                }
+            }
+        }
+
+        private void loadPreset(String name) {
+            mPresetCode = getPresetCode(name);
+            mGlitch.compile(mPresetCode);
+        }
+
+        private String getPresetCode(String name) {
+            File f = new File(mPresetDirPath, name);
+            if (f.exists()) {
+                try {
+                    return new Scanner(f).useDelimiter("\\Z").next();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    return "# Failed to load preset code";
+                }
+            } else {
+                return "Preset file not found";
+            }
+        }
+    }
+
     private class GlitchView extends RenderableView {
+
         private String mScript =
             "# MASSIVE ATTACK - ATLAS AIR\n"
             + "# TODO: make syntax\n"
@@ -164,6 +330,7 @@ public class MainActivity extends Activity {
             "	drumvol * tr808(OH, seq(bpm*2, 0, 0, r()/4+0.1, 0, 0, 0, 0, 0))\n"
             + ")\n"
             + "";
+
         public GlitchView(Context context) {
             super(context);
             mGlitch.compile(mScript);
@@ -179,10 +346,10 @@ public class MainActivity extends Activity {
                 onTextChanged(new SimpleTextWatcher() {
                     public void onTextChanged(CharSequence s) {
                         if (!mGlitch.compile(s.toString())) {
-                            Log.d("Glitch", "syntax: error");
+                            Log.d(TAG, "syntax: error");
                         } else {
                             mScript = s.toString();
-                            Log.d("Glitch", "syntax: ok");
+                            Log.d(TAG, "syntax: ok");
                         }
                     }
                 })),
@@ -191,10 +358,10 @@ public class MainActivity extends Activity {
                 onClick(new OnClickListener() {
                     public void onClick(View v) {
                         if (mAudioThread == null) {
-                            Log.d("Glitch", "Start playback");
+                            Log.d(TAG, "Start playback");
                             // mAudioThread = play();
                         } else {
-                            Log.d("Glitch", "Stop playback");
+                            Log.d(TAG, "Stop playback");
                             // mAudioThread.interrupt();
                             // try {
                             // mAudioThread.join();
