7c7e62fb1a55c86b70f18cb341a43e41dc37c062
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2017-10-08 12:27:21 -0700

    dist: add /i flag for xcopy

--- dist/release-windows.bat
+++ dist/release-windows.bat
@@ -20,8 +20,8 @@ go build -ldflags "-H windowsgui" -o %DISTDIR%\glitch.exe github.com/naivesound/
 
 copy %DIR%\LICENSE %DISTDIR%\LICENSE.txt
 xcopy %DIR%\API.md %DISTDIR%
-xcopy %DIR%\examples %DISTDIR%
-xcopy %DIR%\samples %DISTDIR%
+xcopy /i %DIR%\examples %DISTDIR%\examples
+xcopy /i %DIR%\samples %DISTDIR%\samples
 
 cd %DIR%/dist
 
