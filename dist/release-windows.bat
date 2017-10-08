9851ac452d65d80c8fe1d79e4ffa5738d784e7b3
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2017-10-08 05:18:11 -0700

    dist: add windows batch file to package glitch

---
+++ dist/release-windows.bat
@@ -0,0 +1,26 @@
+@echo off
+
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set UNAME=i686 || set UNAME=amd64
+set PATH=%PATH%;C:\Program Files\7-Zip;C:\Program Files\Git\bin
+
+set VERSION=0.0.0
+
+set DIR=%0\..\..
+set DISTNAME=glitch-windows-%UNAME%-%VERSION%
+set DISTDIR=%DIR%\dist\%DISTNAME%
+
+mkdir %DISTDIR%
+
+go get -u github.com/jteeuwen/go-bindata/...
+go get github.com/naivesound/glitch/cmd/glitch
+go generate github.com/naivesound/glitch/cmd/glitch
+go test github.com/naivesound/glitch/...
+go build -ldflags "-H windowsgui" -o %DISTDIR%\glitch.exe github.com/naivesound/glitch/cmd/glitch
+
+xcopy %DIR%\API.md %DISTDIR%
+xcopy %DIR%\samples %DISTDIR%
+
+cd %DIR%/dist
+
+7z a %DISTNAME%.zip %DISTNAME%
+pause
