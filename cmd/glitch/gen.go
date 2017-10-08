09c6c284409b1ed529f2de1c45f0c8026776b829
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2017-10-08 18:52:20 +0300

    gen: fix bindata relative paths

--- cmd/glitch/gen.go
+++ cmd/glitch/gen.go
@@ -4,7 +4,9 @@ package main
 
 import (
 	"log"
+	"os"
 	"path/filepath"
+	"regexp"
 
 	"github.com/jteeuwen/go-bindata"
 )
@@ -13,18 +15,28 @@ func main() {
 	cfg := bindata.NewConfig()
 	cfg.Package = "main"
 	cfg.Output = "assets.go"
-	uiDir := "../../ui"
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	uiDir, err := filepath.Abs(filepath.Clean(filepath.Join(wd, "../../ui")))
+	if err != nil {
+		log.Fatal(err)
+	}
 	cfg.Prefix = uiDir
-	cfg.Input = []bindata.InputConfig{
-		{Path: filepath.Join(uiDir, "index.html")},
-		{Path: filepath.Join(uiDir, "styles.css")},
-		{Path: filepath.Join(uiDir, "app.js")},
-		{Path: filepath.Join(uiDir, "vendor/codemirror")},
-		{Path: filepath.Join(uiDir, "vendor/roboto-mono")},
-		{Path: filepath.Join(uiDir, "vendor/material-icons")},
-		{Path: filepath.Join(uiDir, "vendor/picodom")},
+	cfg.Input = []bindata.InputConfig{{Path: uiDir, Recursive: true}}
+	cfg.Ignore = []*regexp.Regexp{
+		// Favicons are not used
+		regexp.MustCompile(`favicon\.png`),
+		regexp.MustCompile(`glitch180x180\.png`),
+		regexp.MustCompile(`glitch192x192\.png`),
+		// WebAssembly and asm.js files are not used in desktop variant
+		regexp.MustCompile(`glitch-asm\.js`),
+		regexp.MustCompile(`glitch-asm\.js\.mem`),
+		regexp.MustCompile(`glitch-loader\.js`),
+		regexp.MustCompile(`glitch\.wasm`),
 	}
 	if err := bindata.Translate(cfg); err != nil {
-		log.Fatal(err)
+		log.Fatal("bindata erro:", err)
 	}
 }
