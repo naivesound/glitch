718f1416838911b1fa17e258da20339252b69054
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2017-11-17 21:59:28 +0200

    audio: implement opening/closing midi devices per configuration

--- audio/midi.go
+++ audio/midi.go
@@ -11,70 +11,140 @@ import (
 type MIDICallback func(msg []byte)
 
 type MIDIDevice struct {
-	ID        int
-	Name      string
-	Connected bool
-	midi      rtmidi.MIDIIn
+	ID        int           `json:"id"`
+	Name      string        `json:"name"`
+	Connected bool          `json:"connected"`
+	midi      rtmidi.MIDIIn `json:"-"`
+}
+
+type MIDI interface {
+	SetAutoConnect(bool)
+	AutoConnect() bool
+	SetConnect(name string, connect bool)
+	Devices() []MIDIDevice
+	Destroy()
 }
 
 type midi struct {
 	sync.Mutex
-	cb     MIDICallback
-	wg     *sync.WaitGroup
-	midi   rtmidi.MIDIIn
-	inputs []MIDIDevice
-	exitc  chan struct{}
+	cb          MIDICallback
+	wg          *sync.WaitGroup
+	midi        rtmidi.MIDIIn
+	autoConnect bool
+	connect     map[string]bool
+	inputs      []*MIDIDevice
+	configc     chan struct{}
+	exitc       chan struct{}
+	notify      chan<- struct{}
 }
 
-func NewMIDI(cb MIDICallback) (*midi, error) {
+func NewMIDI(notify chan<- struct{}, cb MIDICallback) (MIDI, error) {
 	midiIn, err := rtmidi.NewMIDIInDefault()
 	if err != nil {
 		return nil, err
 	}
 
 	m := &midi{
-		cb:    cb,
-		wg:    &sync.WaitGroup{},
-		midi:  midiIn,
-		exitc: make(chan struct{}),
+		cb:          cb,
+		wg:          &sync.WaitGroup{},
+		midi:        midiIn,
+		notify:      notify,
+		autoConnect: true,
+		connect:     map[string]bool{},
+		configc:     make(chan struct{}, 1),
+		exitc:       make(chan struct{}),
 	}
 	m.wg.Add(1)
 	go m.poll()
 	return m, nil
 }
 
-func (m *midi) Devices() []MIDIDevice {
+func (m *midi) SetAutoConnect(connect bool) {
 	m.Lock()
 	defer m.Unlock()
-	return m.inputs
+	m.autoConnect = connect
+	m.configc <- struct{}{}
+}
+
+func (m *midi) AutoConnect() bool {
+	m.Lock()
+	defer m.Unlock()
+	return m.autoConnect
+}
+
+func (m *midi) SetConnect(name string, connect bool) {
+	m.Lock()
+	defer m.Unlock()
+	m.connect[name] = connect
+	m.configc <- struct{}{}
+}
+
+func (m *midi) Devices() (devices []MIDIDevice) {
+	m.Lock()
+	defer m.Unlock()
+	for _, d := range m.inputs {
+		devices = append(devices, MIDIDevice{ID: d.ID, Name: d.Name, Connected: d.Connected})
+	}
+	return devices
 }
 
 func (m *midi) Destroy() {
 	close(m.exitc)
+	close(m.configc)
 	m.wg.Wait()
 	m.midi.Close()
+}
+
+func (m *midi) shouldConnect(name string) bool {
+	connect, ok := m.connect[name]
+	return (connect && ok) || (!ok && m.autoConnect)
+}
+
+func (m *midi) openDevice(d *MIDIDevice) (err error) {
+	if d.midi, err = rtmidi.NewMIDIInDefault(); err != nil {
+		return err
+	} else if err = d.midi.OpenPort(d.ID, d.Name); err != nil {
+		return err
+	} else {
+		d.Connected = true
+		d.midi.SetCallback(func(_ rtmidi.MIDIIn, msg []byte, _ float64) {
+			m.cb(msg)
+		})
+	}
+	return nil
+}
+
+func (m *midi) closeDevice(d *MIDIDevice) {
+	d.Connected = false
+	d.midi.CancelCallback()
+	d.midi.Close()
 }
 
 func (m *midi) poll() {
 	defer m.wg.Done()
 	for {
-		if err := m.pollOnce(); err != nil {
+		if changed, err := m.pollOnce(); err != nil {
 			log.Println(err)
+		} else if changed {
+			m.notify <- struct{}{}
 		}
 		select {
 		case <-m.exitc:
 			return
+		case <-m.configc:
 		case <-time.After(time.Second):
 		}
 	}
 }
 
-func (m *midi) pollOnce() error {
+func (m *midi) pollOnce() (changed bool, err error) {
 	m.Lock()
 	defer m.Unlock()
+
+	// Get a list of currently connected devices
 	n, err := m.midi.PortCount()
 	if err != nil {
-		return err
+		return changed, err
 	}
 	names := make([]string, n, n)
 	for i := 0; i < n; i++ {
@@ -83,39 +153,52 @@ func (m *midi) pollOnce() error {
 			names[i] = name
 		}
 	}
-	inputs := []MIDIDevice{}
+	inputs := []*MIDIDevice{}
+
+	// Close and remove disconnected devices
 	for _, d := range m.inputs {
 		if d.ID < len(names) && d.Name == names[d.ID] {
 			inputs = append(inputs, d)
 		} else {
-			d.Connected = false
-			d.midi.CancelCallback()
-			d.midi.Close()
+			m.closeDevice(d)
+			changed = true
 		}
 	}
+
+	// Add newly connected devices and change connection status if needed
 	m.inputs = inputs
 	for i := 0; i < n; i++ {
-		found := false
-		// TODO: check if whitelisted
+		var device *MIDIDevice
 		for _, d := range m.inputs {
 			if d.ID == i && d.Name == names[i] {
-				found = true
+				device = d
 				break
 			}
 		}
-		if !found {
-			if in, err := rtmidi.NewMIDIInDefault(); err != nil {
-				return err
-			} else if err := in.OpenPort(i, names[i]); err != nil {
-				return err
-			} else {
-				d := MIDIDevice{ID: i, Name: names[i], Connected: true, midi: in}
-				d.midi.SetCallback(func(_ rtmidi.MIDIIn, msg []byte, _ float64) {
-					m.cb(msg)
-				})
-				m.inputs = append(m.inputs, d)
+
+		if device != nil {
+			if device.Connected && !m.shouldConnect(device.Name) {
+				// Existing device should be disconnected
+				m.closeDevice(device)
+				changed = true
+			} else if !device.Connected && m.shouldConnect(device.Name) {
+				// Existing device should be connected
+				if err := m.openDevice(device); err != nil {
+					return changed, err
+				}
+				changed = true
 			}
+		} else {
+			// New device should be connected
+			device = &MIDIDevice{ID: i, Name: names[i], Connected: m.shouldConnect(names[i])}
+			if device.Connected {
+				if err := m.openDevice(device); err != nil {
+					return changed, err
+				}
+				changed = true
+			}
+			m.inputs = append(m.inputs, device)
 		}
 	}
-	return nil
+	return changed, nil
 }
